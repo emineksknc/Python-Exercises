3489712cff86b0aece654fd3af15c446c04c05d1
emineksknc <eminekskncc@gmail.com>
2023-03-13 15:08:46 +0300

    Python-basic-(Part -I)- Solution - 5

---
+++ Python-basic-(Part -I)/5.py
@@ -0,0 +1,5 @@
+# 5. Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+
+name = input("Name: ")
+surname = input("Surname: ")
+print("Hello", surname, name)
