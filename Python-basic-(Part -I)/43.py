cf5b27c61f9a3272eeffb7fd6792e9c71d12d981
emineksknc <eminekskncc@gmail.com>
2023-03-16 00:32:05 +0300

    Python-basic-(Part -I)- Solution - 43

---
+++ Python-basic-(Part -I)/43.py
@@ -0,0 +1,7 @@
+# 43. Write a Python program to get OS name, platform and release information.
+
+import platform
+
+print("OS Name  : ", platform.system())
+print("Platform : ", platform.platform())
+print("Release  : ", platform.release())
