46ebffbe63512024048adb26e0334ecf1d1fa295
emineksknc <eminekskncc@gmail.com>
2023-03-15 23:28:55 +0300

    Python-basic-(Part -I)- Solution - 37

---
+++ Python-basic-(Part -I)/37.py
@@ -0,0 +1,11 @@
+# 7. Write a Python program that displays your name, age, and address on three different lines
+
+print("/***     REGISTRY PAGE      ***/")
+name = input("Name : ")
+age = input("Age : ")
+address = input("Address : ")
+print("Your registry are completed!")
+
+print("\n *** Name: ", name)
+print("\n *** Age: ", age)
+print("\n *** Address: ", address)
