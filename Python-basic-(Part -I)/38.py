8f5e1a5dc8f138597632926779cbd7a1c766ea37
emineksknc <eminekskncc@gmail.com>
2023-03-15 23:32:16 +0300

    Python-basic-(Part -I)- Solution - 38

---
+++ Python-basic-(Part -I)/38.py
@@ -0,0 +1,10 @@
+# 38. Write a Python program to solve (x + y) * (x + y).
+
+def calculate_square_of_summation(x, y):
+    return (x + y) ** 2
+
+
+x = 4
+y = 3
+
+print(calculate_square_of_summation(x, y))
