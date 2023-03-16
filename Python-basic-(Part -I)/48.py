9a52ecca6a0d12a1401748b0c0abde8effd18c9e
emineksknc <eminekskncc@gmail.com>
2023-03-16 18:20:18 +0300

    Python-basic-(Part -I)- Solution - 48

---
+++ Python-basic-(Part -I)/48.py
@@ -0,0 +1,10 @@
+# 48. Write a Python program to parse a string to float or integer.
+
+
+str_pi = "3"
+float_pi = float(str_pi)
+int_pi = int(str_pi)
+
+print(str_pi, type(str_pi))
+print(float_pi, type(float_pi))
+print(int_pi, type(int_pi))
