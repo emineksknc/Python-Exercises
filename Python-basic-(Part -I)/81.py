025022d62751b0ff8190fe702849e6348ddb683c
emineksknc <eminekskncc@gmail.com>
2023-03-21 15:48:45 +0300

    Python-basic-(Part -I)- Solution - 81

---
+++ Python-basic-(Part -I)/81.py
@@ -0,0 +1,7 @@
+# 81. Write a Python program to concatenate N strings.
+
+list_letter = ["H", "e", "l", "l", "o", "W", "o", "r", "l", "d", "!"]
+list_word = ["Hi", "!"]
+
+print("_".join(list_letter))
+print("->".join(list_word))
