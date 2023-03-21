6c5fc03bf98ef35989d5d01cdf6e0b9903749f8b
emineksknc <eminekskncc@gmail.com>
2023-03-21 12:32:29 +0300

    Python-basic-(Part -I)- Solution - 74

---
+++ Python-basic-(Part -I)/74.py
@@ -0,0 +1,9 @@
+# 74. Write a Python program to hash a word
+
+integer_num = 12
+float_num = 43.2
+word = "Lorem"
+
+print(f"Integer ({integer_num}) : ", hash(integer_num))
+print(f"Float ({float_num}) : ", hash(float_num))
+print(f"Word ({word}) : ", hash(word))
