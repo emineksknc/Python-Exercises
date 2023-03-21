1e0c4d5e09171e31fd0950fe2ab088c55e6cd49f
emineksknc <eminekskncc@gmail.com>
2023-03-21 12:37:28 +0300

    Python-basic-(Part -I)- Solution - 75

---
+++ Python-basic-(Part -I)/75.py
@@ -0,0 +1,8 @@
+# 75. Write a Python program to get the copyright information and write Copyright information in Python code.
+
+__author__ = "Software Authors Name"
+__copyright__ = "Copyright (C) 2004 Author Name"
+__license__ = "Public Domain"
+__version__ = "1.0"
+
+print(f"Author: {__author__} \nCopyright : {__copyright__} \nLisense : {__license__} \nVersion : {__version__} \n ")
