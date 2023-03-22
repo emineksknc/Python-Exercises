1486d70c40d4dc7bc5f40c886f01f4fcd84b8b6b
emineksknc <eminekskncc@gmail.com>
2023-03-22 18:53:25 +0300

    Python-basic-(Part -I)- Solution - 93

---
+++ Python-basic-(Part -I)/93.py
@@ -0,0 +1,8 @@
+# 93. Write a Python program to get the Identity, Type, and Value of an object
+"""
+https://www.digitalocean.com/community/tutorials/python-id
+"""
+
+object = "string"
+
+print(f"Value: {object} \nIdentity : {id(object)} \nType : {type(object)}")
